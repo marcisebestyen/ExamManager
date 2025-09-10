2b37b1184d8bfa8e56d4c4a2f8cdbcaa6ae519a5
Marcell Sebestyen <143648545+marcisebestyen@users.noreply.github.com>
2025-09-10 12:26:33 +0200

    redoxd

--- ExamManager/exam-manager-frontend/vite.config.mjs
+++ ExamManager/exam-manager-frontend/vite.config.mjs
@@ -3,10 +3,22 @@ import { defineConfig } from 'vite';
 import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
     setupFiles: './vitest.setup.mjs',
+  },
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://localhost:7195',
+        changeOrigin: true,
+        secure: false
+      }
+    },
+    port: 7285,
+    open: true,
+    host: 'localhost'
   },
 });
